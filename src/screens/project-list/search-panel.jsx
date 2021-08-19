5b5fc950aeecb68cab81eb31486593c9b2d5da31
qiii <646927106@qq.com>
2021-08-19 17:56:36 +0800

    feat：添加工程列表

---
+++ src/screens/project-list/search-panel.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const SearchPanel = ({param, users, setParam}) => {
+	return <form>
+		<input type="text" value={param.name} onChange={evt => setParam({
+			...param,
+			name: evt.target.value
+		})}/>
+		<select value={param.personId} onChange={evt => setParam({
+			...param,
+			personId: evt.target.value
+		})}>
+			<option value={''}>负责人</option>
+			{
+				users.map(user => <option key={user.id} value={user.id}>{user.name}</option>)
+			}
+		</select>
+	</form>
+}
