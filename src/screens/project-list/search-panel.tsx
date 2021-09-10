9845b818c1f940cd9145ba527e6c68234d2c365c
孙麒 <sunq153@chinaunicom.cn>
2021-09-10 10:05:55 +0800

    style: 安装emotion，优化登录注册界面

--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Input, Select} from "antd";
 
 export interface User {
   id: string;
@@ -10,28 +11,28 @@ export interface User {
 }
 
 interface SearchPanelProps {
-    users: User[];
-    param: {
-        name: string;
-        personId: string;
-    };
-    setParam: (param: SearchPanelProps['param']) => void;
+  users: User[];
+  param: {
+    name: string;
+    personId: string;
+  };
+  setParam: (param: SearchPanelProps['param']) => void;
 }
 
 export const SearchPanel = ({param, users, setParam}: SearchPanelProps) => {
-	return <form>
-		<input type="text" value={param.name} onChange={evt => setParam({
-			...param,
-			name: evt.target.value
-		})}/>
-		<select value={param.personId} onChange={evt => setParam({
-			...param,
-			personId: evt.target.value
-		})}>
-			<option value={''}>负责人</option>
-			{
-				users.map(user => <option key={user.id} value={user.id}>{user.name}</option>)
-			}
-		</select>
-	</form>
+  return <form>
+    <Input type="text" value={param.name} onChange={evt => setParam({
+      ...param,
+      name: evt.target.value
+    })}/>
+    <Select value={param.personId} onChange={value => setParam({
+      ...param,
+      personId: value
+    })}>
+      <Select.Option value={''}>负责人</Select.Option>
+      {
+        users.map(user => <Select.Option key={user.id} value={user.id}>{user.name}</Select.Option>)
+      }
+    </Select>
+  </form>
 }
